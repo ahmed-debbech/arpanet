3de92be4c3b1903f982ec0de1e2882f7eab1bbf7
ahmed-debbech <debbech.ahmed@gmail.com>
2023-09-29 12:30:21 +0100

    fix bug in string compying

--- main.c
+++ main.c
@@ -7,13 +7,14 @@ void request(int size, struct interfaces ** a){
     for(i=0; i<=size-1; i++){
         printf("[%d] %s \t %s\n", i+1, a[i]->name, a[i]->subnet);
     }
-    
+    int input;
+    scanf("%d", &input);
+    printf("picking %s ..\n", a[input-1]->name);
     return;
 }
 int main (){
     struct interfaces ** a;
     int size = getAllInterfaces(&a);
-
     request(size, a);
     return 0;
 }
--- network.c
+++ network.c
@@ -4,27 +4,34 @@
 #include "network.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getAllInterfaces(struct interfaces *** arr){
     struct ifaddrs *ifap, *ifa;
     struct sockaddr_in *sa;
-    char *addr;
     getifaddrs (&ifap);
 
     int i =0;
-    for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
+    ifa = ifap;
+    while (ifa != NULL) {
         if (ifa->ifa_addr->sa_family==AF_INET) {
             sa = (struct sockaddr_in *) ifa->ifa_netmask;
-            addr = inet_ntoa(sa->sin_addr);
+            char * addr =  inet_ntoa(sa->sin_addr);
             struct interfaces * inter = malloc(sizeof(struct interfaces));
             inter->name = ifa->ifa_name;
-            inter->subnet = addr;
+            char * addr2 = (char*) malloc(sizeof(char) * (strlen(addr)-1));
+            int y=0;
+            for(y=0; y<=strlen(addr)-1; y++){
+                addr2[y] = addr[y];
+            }
+            addr2[y]='\0';
+            inter->subnet = addr2;
             *arr = realloc(*arr, (i+1)*  sizeof(struct interfaces *));
             (*arr)[i] = inter;
             i++;
         }
+        ifa = ifa->ifa_next;
     }
-
     freeifaddrs(ifap);
     return i-1;
 }
